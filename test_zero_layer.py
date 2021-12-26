6f74a710adf83580a7d4912deac34eb2910b2509
Elana Simon <elana@reverielabs.com>
2021-12-25 21:50:59 -0800

    test for backward pass of embedder

--- test_zero_layer.py
+++ test_zero_layer.py
@@ -8,8 +8,16 @@ def test_embedder():
     n_vocab = 3
     d_model = 2
     embedder = Embedder(n_vocab=n_vocab, d_model=d_model)
-    embedder.param = torch.nn.Parameter(torch.tensor([[1, 1], [10, 10], [100, 100]]))
-    tokens = [1, 2, 1]
+    embedder.param = torch.nn.Parameter(
+        torch.tensor([[1.0, 1.0], [10.0, 10.0], [100.0, 100.0]])
+    )
+    tokens = torch.tensor([1, 2, 1])
 
     acts = embedder.forward(tokens)
-    assert acts.tolist() == [[10, 10], [100, 100], [10, 10]]
+    assert acts.tolist() == [[10.0, 10.0], [100.0, 100.0], [10.0, 10.0]]
+
+    act_grads = torch.tensor([[1.0, 1.0], [2.0, 2.0], [3.0, 4.0]])
+    embedder.backward(act_grads)
+    assert torch.allclose(
+        embedder.param_grads, torch.tensor([[0.0, 0.0], [4.0, 5.0], [2.0, 2.0]])
+    )
