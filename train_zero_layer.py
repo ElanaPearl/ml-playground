eacf6f2b8eaa52d286558e47915ea2044386dcab
Elana Simon <elana@reverielabs.com>
2022-01-02 20:33:23 -0800

    wrote backward for softmax

--- train_zero_layer.py
+++ train_zero_layer.py
@@ -1,4 +1,4 @@
-from torch._C import TensorType
+import torch
 from torch.functional import Tensor
 
 from optimizers import Adam
@@ -11,15 +11,31 @@ from zero_layer_transformer import (
 )
 
 
-def softmax(logits: Logits) -> Tensor:
-    logits -= logits.max()
-    exp_logits = logits.exp()
-    return exp_logits / exp_logits.sum()
+class Softmax:
+    def forward(self, logits: Logits) -> Tensor:
+        # subtract mean for numeric stability, doesn't affect results
+        normalized_logits = logits - logits.max()
+        exp_logits = normalized_logits.exp()
+        self.probs = exp_logits / exp_logits.sum()
+        return self.probs
+
+    def backward(self, softmax_grads: Tensor) -> Logits:
+        """Equation to calculate derivative of softmax:
+        dL/dx_i = dL/d_Si * dS_i/dx_i + ∑_{i!=j} dL/d_Sj * dS_j/dx_i
+        where S_i = softmax(x_i) and:
+            dS_i/dx_i = S_i(1 - S_i)
+            dS_i/dx_j = S_i*S_j
+        """
+
+        kronecker_delta = torch.eye(self.probs.shape[2])
+        outer = torch.einsum("bnv,bnw->bnvw", self.probs, self.probs)
+        jacobian = -outer + torch.einsum("vw,bnv->bnvw", kronecker_delta, self.probs)
+        return torch.einsum("bnvw,bnv->bnv", jacobian, softmax_grads)
 
 
 class SoftmaxCrossEntropyLoss:
     def forward(self, logits: Logits, labels: Tokens) -> Tensor:
-        probs = softmax(logits)
+        probs = Softmax.forward(logits)
         # probs are BxNxV but labels are BxN so labels need an extra dim for gather
         labels_3d = labels[:, :, None]
         predicted_probs_for_labels = probs.gather(dim=2, index=labels_3d)
